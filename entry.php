f4f793dba2fa1ca891e90107e8157fbbca3d0efb
Jackthehaxk21 <gangnam253@gmail.com>
2020-03-23 16:52:11 +0000

    Big change internally on exit codes. v0.1.0 - RC

--- entry.php
+++ entry.php
@@ -1,34 +1,54 @@
 #!/usr/bin/env php
 <?php
 
+/**
+ * Entry.php
+ *
+ * Specific Exit Codes:
+ * 0 - N/A (no errors, analysis complete OK).
+ * 1 - 'Catchall for general errors'.
+ * 2 - 'Misuse of shell builtins'.
+ * ---
+ * 3 - Unable to extract plugin.
+ * 4 - Unable to parse plugin.
+ * 5 - Composer failed.
+ * 6 - Analysis failed/found problems.
+ * 7 - PHPStan failed to parse the plugin.
+ * ---
+ * 9 - Unknown error.
+ */
+
 declare(strict_types=1);
 
-echo "Extracting plugin from {$_ENV["PLUGIN_FILE"]}...\n";
+echo "[Info] -> Extracting plugin from {$_ENV["PLUGIN_FILE"]}...\n";
 
 $source = "/source/";
 
 try{
-	$phar = new Phar("/source/{$_ENV["PLUGIN_FILE"]}");
-	$phar->extractTo("/source/");
+    $phar = new Phar("/source/{$_ENV["PLUGIN_FILE"]}");
+    $phar->extractTo("/source/");
 } catch (\Exception $e){
-	echo "[ERROR] -> Failed to extract {$_ENV["PLUGIN_FILE"]}\n\n";
-	echo $e->getMessage();
-	exit(1);
+    echo "[Error] -> Failed to extract {$_ENV["PLUGIN_FILE"]}\n\n";
+    echo $e->getMessage();
+    exit(3);
 }
 
-echo "Starting prerequisite checks...\n";
+echo "[Info] -> Starting prerequisite checks...\n";
 
 if(is_file($source."phpstan.neon.dist")) $_ENV["PHPSTAN_CONFIG"] = "/source/phpstan.neon.dist";
-
 if(is_file($source."phpstan.neon")) $_ENV["PHPSTAN_CONFIG"] = "/source/phpstan.neon";
 
 if(is_file($source."plugin.yml")) {
     if(!is_dir($source."src")) {
-        echo "src not found in '{$source}'. Are the paths set correctly?\n";
-        exit(1);
+        echo "[Error] -> src not found in '{$source}'. Are the paths set correctly?";
+        exit(4);
     }
 
     $manifest = yaml_parse(file_get_contents($source."plugin.yml"));
+    if(!$manifest){
+        echo "[Error] -> Failed to parse plugin.yml";
+        exit(4);
+    }
     $deps = [];
     foreach(["depend", "softdepend", "loadbefore"] as $attr) {
         if(isset($manifest[$attr])) {
@@ -41,10 +61,10 @@ if(is_file($source."plugin.yml")) {
             continue;
         }
 
-        echo "Attempting to download dependency $dep from Poggit...\n";
+        echo "[Info] -> Attempting to download dependency $dep from Poggit...\n";
         $code = pclose(popen("wget -O /deps/$dep.phar https://poggit.pmmp.io/get/$dep", "r"));
         if($code !== 0) {
-            echo "Warning: Failed to downloading dependency $dep\n";
+            echo "[Warning] -> Failed to downloading dependency $dep\n";
             // still continue executing
         }
     }
@@ -53,15 +73,29 @@ if(is_file($source."plugin.yml")) {
 if(is_file($source."composer.json")) {
     passthru("composer install --no-suggest --no-progress -n -o", $result);
     if($result !== 0) {
-        echo "Failed to install composer dependencies.\n";
-        exit(1);
+        echo "[Error] -> Failed to install composer dependencies.\n";
+        exit(5);
     }
 }
 
-echo "Starting phpstan...\n";
+echo "[Info] -> Starting phpstan...\n";
 
 $proc = proc_open("phpstan analyze --error-format=json --no-progress --memory-limit=2G -c {$_ENV["PHPSTAN_CONFIG"]} > /source/phpstan-results.json", [["file", "/dev/null", "r"], STDOUT, STDERR], $pipes);
 if(is_resource($proc)) {
     $code = proc_close($proc);
-    exit($code);
+    if($code === 1){
+        echo "[Warning] -> Analysis failed/found problems.";
+        exit(6);
+    }
+    if($code === 255){
+        //Phpstan unable to parse, shouldn't happen...
+        echo "[Error] -> PHPStan (255) - Unable to parse.";
+        exit(7);
+    }
+    if($code !== 0) {
+        echo "[Error] -> Unhandled exit status: $code";
+        exit(9);
+    }
+    echo "[Info] -> No problems found !";
+    exit(0);
 }
